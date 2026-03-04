4bbb494197def4fb36349f070c4270d7546a4f50
Tonmoy chakrabarty Joy <tonmoy.chakrabarty.joy01@gmail.com>
2026-03-04 23:24:53 +0600

    Add files via upload

--- BackgroundPanel.java
+++ BackgroundPanel.java
@@ -4,18 +4,20 @@ import java.awt.*;
 public class BackgroundPanel extends JPanel {
     private Image backgroundImage;
 
-    public BackgroundPanel(String imagePath) {
-        try {
-            backgroundImage = new ImageIcon(imagePath).getImage();
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, "Could not load background image: " + imagePath);
+    public BackgroundPanel(String path) {
+        if(path != null && !path.isEmpty()) {
+            ImageIcon icon = new ImageIcon(path);
+            backgroundImage = icon.getImage();
+            if(icon.getIconWidth() <= 0){
+                System.out.println("Cannot load image: " + path);
+            }
         }
     }
 
-
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (backgroundImage != null) {
+        if(backgroundImage != null) {
             g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
         }
     }
